d74aad4cd8a133e671764dbdfba8f775d2db1164
alswns6327 <alswnsdl333@gmail.com>
2023-12-24 21:15:20 +0900

    회원가입, 로그인, 로그아웃

--- src/main/java/org/mj/blog/controller/UserApiController.java
+++ src/main/java/org/mj/blog/controller/UserApiController.java
@@ -1,8 +1,12 @@
 package org.mj.blog.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.mj.blog.dto.AddUserRequest;
 import org.mj.blog.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -17,5 +21,12 @@ public class UserApiController {
     public String singup(AddUserRequest request) {
         userService.save(request); // 회원 가입 메서드 호출
         return "redirect:/login"; // 회원가입이 완료된 이후 로그인 페이지 이동
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        new SecurityContextLogoutHandler().logout(request, response,
+                SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 }
