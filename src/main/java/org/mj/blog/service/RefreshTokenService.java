456072d034c280e688c16253209f25666ffb5a2a
alswns6327 <alswnsdl333@gmail.com>
2023-12-25 16:42:21 +0900

    refreshToken 활용한 accessToken 생성 로직 구현

---
+++ src/main/java/org/mj/blog/service/RefreshTokenService.java
@@ -0,0 +1,17 @@
+package org.mj.blog.service;
+
+import lombok.RequiredArgsConstructor;
+import org.mj.blog.domain.RefreshToken;
+import org.mj.blog.repository.RefreshTokenRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken){
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+}
